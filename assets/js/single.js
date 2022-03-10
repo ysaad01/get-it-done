841084ff362ee2fc2e7cc5099b863759134e6837
ysaad01 <ysaad2325@gmail.com>
2022-03-10 17:20:01 -0500

    display warning function added for repos with over 30 open issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 /* variables */
 var issueContainerEl = document.querySelector("#issues-container")
+var limitWarningEl = document.querySelector("#limit-warning")
 
 /* functions */
 var getRepoIssues = function (repo) {
@@ -12,6 +13,11 @@ var getRepoIssues = function (repo) {
       response.json().then(function (data) {
         /* pass response data to dom function */
         displayIssues(data);
+
+        /* check if api has paginated issues */
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
       });
     } else {
       alert("There was a problem with your request!");
@@ -55,6 +61,19 @@ var displayIssues = function (issues) {
 
     issueContainerEl.appendChild(issueEl)
   }
+};
+
+var displayWarning = function (repo) {
+  /* add text to warning container */
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com"
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  /* append to warning container */
+  limitWarningEl.appendChild(linkEl);
 };
 
 getRepoIssues("facebook/react");
